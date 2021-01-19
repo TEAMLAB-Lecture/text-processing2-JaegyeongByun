dbc71b07caac5f3997705035bb5139ff00d1c1b5
JaegyeongByun <worud1208@gmail.com>
2021-01-19 19:45:54 +0900

    Update my code

--- text_processing2.py
+++ text_processing2.py
@@ -28,7 +28,17 @@ def digits_to_words(input_string):
             >>> tp2.digits_to_words(digits_str2)
             'three one four one five'
     """
-    digit_string = None
+
+    digit_string = ''
+    digit = {'0': 'zero', '1': 'one', '2':'two', 
+            '3':'three', '4':'four', '5':'five', '6':'six', 
+            '7': 'seven', '8': 'eight', '9': 'nine'}
+    
+    for string in input_string:
+        if string in digit:
+            digit_string += digit[string] + ' '
+
+    digit_string = digit_string[:-1]
     return digit_string
 
 
@@ -64,5 +74,22 @@ def to_camel_case(underscore_str):
             >>> tp2.to_camel_case(underscore_str3)
             "alreadyCamel"
     """
-    camelcase_str = None
+    if '_' not in underscore_str:
+        return underscore_str
+
+    camelcase_str = ''
+    isfirst = True
+    pre_is_underscore = False
+    for string in underscore_str:
+        if string == '_':
+            pre_is_underscore = True
+
+        else:            
+            if isfirst == False and pre_is_underscore == True:
+                camelcase_str += string.upper()
+            else:
+                camelcase_str += string.lower()
+            pre_is_underscore = False
+            isfirst = False
+
     return camelcase_str
